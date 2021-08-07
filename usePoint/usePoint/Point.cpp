c8a27e0c126c3eecd741fd5887b985660fa76867
user0324 <etu.leti0324@gmail.com>
2021-08-07 21:55:55 +0000

    пока разбираюсь

---
+++ usePoint/usePoint/Point.cpp
@@ -0,0 +1,11 @@
+// Реализация класса Point
+/*#include "Point.h"
+
+void  Point::SetX(int _x)
+{
+	x = _x >= 0 ? _x : 0;
+}
+void Point::SetY(int)
+{
+	y = _y >= 0 ? _y : 0;
+}*/
---
+++ usePoint/usePoint/Point.h
@@ -0,0 +1,16 @@
+// Определение класса Point
+#pragma once
+/*#ifdef POINT_H
+#define POINT_H
+class Point
+{
+private:
+	int x, y;
+public:
+	int GetX() { return x; } //inline по умолчанию
+	int GetY();
+};
+
+inline int Point::GetY() { return y; }
+
+#endif // POINT_H*/
